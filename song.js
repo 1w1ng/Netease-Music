8589e8f4e8a5336df1d0757e7d4cdc41ef0ef47c
1w1ng <921754984@qq.com>
2018-05-02 10:21:18 +0800

    完善 播放页面 逻辑

--- song.js
+++ song.js
@@ -1,38 +1,62 @@
 $(function(){ 
-  $.get('/lyric.json').then(function(object){
-    let {lyric} = object
+
+  let id = location.search.match(/\bid=([^&]*)/)[1]
+
+  $.get('./songs.json').then(function(response){
+    let songs = response
+    let song = songs.filter(s => s.id == id)[0]
+    let {url,name,lyric} = song
+
+    initPlayer.call(undefined,url)
+    initText(name,lyric)
+  })
+
+  function initText(name,lyric){
+    $('.song-description > h1').text(name)
+    parseLyric(lyric)
+  }
+
+  function initPlayer(url){
+    // 开始播放音乐
+    let audio = document.createElement('audio')
+    audio.src = url
+    audio.oncanplay = function () {
+      audio.play()
+      $('.disc-container').addClass('playing')
+    }
+    // 暂停播放功能
+    $('.icon-pause').on('click', function () {
+      audio.pause()
+      $('.disc-container').removeClass('playing')
+    })
+    $('.icon-play').on('click', function () {
+      audio.play()
+      $('.disc-container').addClass('playing')
+    })
+  }
+
+  // $.get('/lyric.json').then(function(object){
+  //   let {lyric} = object
+  //   parseLyric.call(undefined,lyric)
+  // })
+
+  function parseLyric(lyric){
     let array = lyric.split('\n')
     let regex = /^\[(.+)\](.*)$/
-    array = array.map(function(string,index){
+    array = array.map(function (string, index) {
       let matches = string.match(regex)
-      if(matches){
-        return { time: matches[1],words: matches[2] }
+      if (matches) {
+        return { time: matches[1], words: matches[2] }
       }
     })
     let $lyric = $('.lyric')
-    array.map(function(object){
-      if(!object){
+    array.map(function (object) {
+      if (!object) {
         return
       }
       let $p = $('<p/>')
-      $p.attr('data-time',object.time).text(object.words)
+      $p.attr('data-time', object.time).text(object.words)
       $p.appendTo($lyric.children('.lines'))
     })
-  })
-  // 进入页面开始播放音乐
-  let audio = document.createElement('audio')
-  audio.src = '//p7zhcxqif.bkt.clouddn.com/music.mp3'
-  audio.oncanplay = function(){
-    audio.play()
-    $('.disc-container').addClass('playing')
   }
-  // 暂停播放功能
-  $('.icon-pause').on('click',function(){
-    audio.pause()
-    $('.disc-container').removeClass('playing')
-  })
-  $('.icon-play').on('click', function () {
-    audio.play()
-    $('.disc-container').addClass('playing')
-  })
 })
